dae5d787b139539044777bb0004226087c346548
guiyomi98 <guiyomi98@gmail.com>
2021-11-01 14:30:05 +0900

    클래스로 변경

--- src/List.js
+++ src/List.js
@@ -1,16 +1,24 @@
 import React from "react";
+import PropTypes from "prop-types";
+import './List.css';
 
-function List(props) {
-    console.log(props)
-    return (
-        <ul>
-            <li>
-                <strong></strong>
-                <p></p>
-                <img src="#" alt="#"></img>
-            </li>
-        </ul>
-    )
+function List({ id, rating, title, poster, summary }) {
+  return (
+    <li className="list__item">
+      <h4 className="title">{ title }</h4>
+      <img className="img" src={poster} alt={title} title={title} />
+      <p className="rating"><span>{ rating }</span>/10</p>
+      <p className="descript">{summary.slice(0.180)}...</p>
+    </li>
+  )
+}
+
+List.propTypes = {
+  id: PropTypes.number.isRequired,
+  rating: PropTypes.number.isRequired,
+  title: PropTypes.string.isRequired,
+  poster: PropTypes.string.isRequired,
+  summary: PropTypes.string.isRequired
 }
 
 export default List;
