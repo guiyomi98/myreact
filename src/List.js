3df621c7d079d97763206f3961bfd96d2d12e883
guiyomi98 <guiyomi98@gmail.com>
2021-11-02 13:36:28 +0900

    스타일 sass로 변경

--- src/List.js
+++ src/List.js
@@ -1,14 +1,14 @@
 import React from "react";
 import PropTypes from "prop-types";
-import './List.css';
+import './List.scss';
 
 function List({ id, rating, title, poster, summary }) {
   return (
     <li className="list__item">
-      <h4 className="title">{ title }</h4>
-      <img className="img" src={poster} alt={title} title={title} />
+      <h3 className="title">{ title }</h3>
+      <img className="img" src={ poster } alt={ title } title={ title } />
       <p className="rating"><span>{ rating }</span>/10</p>
-      <p className="descript">{summary.slice(0.180)}...</p>
+      <p className="descript">{ summary }</p>
     </li>
   )
 }
